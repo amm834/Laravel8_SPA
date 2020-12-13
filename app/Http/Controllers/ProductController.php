ab51ef29ed986d9be2e8bf8e50f37f48d7d2b357
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2020-12-13 21:15:12 +0630

    CRUD:Added Vliadtion

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\Validator;
+
 class ProductController extends Controller
 {
   /**
@@ -22,8 +24,16 @@ class ProductController extends Controller
   * @return \Illuminate\Http\Response
   */
   public function store(Request $request) {
-    $product = Product::create($request->only('name', 'price'));
-    return $product;
+    $validator = Validator::make($request->all(), [
+      'name' => 'required|string',
+      'price' => 'required|numeric'
+    ]);
+    if ($validator->fails()) {
+      return $validator->errors();
+    } else {
+      $product = Product::create($request->only('name', 'price'));
+      return $product;
+    }
   }
 
   /**
@@ -45,9 +55,17 @@ class ProductController extends Controller
   * @return \Illuminate\Http\Response
   */
   public function update(Request $request, $id) {
+        $validator = Validator::make($request->all(), [
+      'name' => 'nullable|string',
+      'price' => 'nullable|numeric'
+    ]);
+    if($validator->fails()){
+      return $validator->errors();
+    }else{
     $product = Product::find($id);
-    $product->update($request->only('name','price'));
+    $product->update($request->only('name', 'price'));
     return $product;
+    }
   }
 
   /**
@@ -59,6 +77,6 @@ class ProductController extends Controller
   public function destroy($id) {
     $product = Product::find($id);
     $product->delete();
-    return $product; 
+    return $product;
   }
 }
